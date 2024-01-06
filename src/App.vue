61677759acf90c1c7ce8210ed237dbf6fb745e7b
Gabriel Alves <gabriel52.alves@gmail.com>
2024-01-06 19:57:42 -0300

    feat: Header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,32 @@
 <template>
-  <div> Start </div>
+  <body>
+    <Header></Header>
+  </body>
 </template>
 
 <script>
+import Header from './components/HeaderMain.vue';
+import './assets/style/global.css'
 
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+  }
+}
 </script>
 
 <style>
-
+/* #app {
+   font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50; 
+  margin-top: 60px; 
+} */
+body {
+  background-color: #E8EBE4;
+}
 </style>
---
+++ src/components/HeaderMain.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="header">
+        <p class="text-header">Qr-code generator</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HeaderMain'
+    }
+</script>
+
+<style>
+    .header {
+        display: flex;
+        width: 100%;
+        height: 7vh;
+        background-color: #002F40;
+        border-bottom: 0.5vh solid;
+        border-image: linear-gradient(60deg, #A31621, #00A878) 1 1;
+    }
+    .text-header {
+        color: #F8F8F8;
+    }
+
+</style>
